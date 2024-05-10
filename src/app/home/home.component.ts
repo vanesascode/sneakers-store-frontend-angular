b01bbfd020ae4d6e8fcd92e24321048b16c3efb1
vanesa <vanesa.juarez.paris@gmail.com>
2024-05-10 19:27:18 +0200

    feat: :sparkles: fetch the list of products with the getProducts method in the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit() {
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products.items);
+      });
+  }
+}
