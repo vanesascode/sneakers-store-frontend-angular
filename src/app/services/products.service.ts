c4cff914adad7ad5729ce0254c8178a396d272d1
vanesa <vanesa.juarez.paris@gmail.com>
2024-05-30 12:54:32 +0200

    feat: :sparkles: add the add, edit and delete product services in the products service file

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -14,4 +14,16 @@ export class ProductsService {
   ): Observable<Products> => {
     return this.apiService.get(url, { params, responseType: 'json' });
   };
+
+  addProduct = (url: string, body: any): Observable<any> => {
+    return this.apiService.post(url, body, {});
+  };
+
+  editProduct = (url: string, body: any): Observable<any> => {
+    return this.apiService.put(url, body, {});
+  };
+
+  deleteProduct = (url: string): Observable<any> => {
+    return this.apiService.delete(url, {});
+  };
 }
