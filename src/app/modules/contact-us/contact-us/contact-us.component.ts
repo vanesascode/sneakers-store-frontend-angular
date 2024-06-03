d13380b5cd115c89b9367d90faec53eeecfa4bcc
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-03 19:46:58 +0200

    feat: :sparkles: add the form logic in the contact-us component file

--- src/app/modules/contact-us/contact-us/contact-us.component.ts
+++ src/app/modules/contact-us/contact-us/contact-us.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { createInvalidDomainValidator } from '../contact-us/invalidEmailDomain';
+
+const invalidEmailDomain = createInvalidDomainValidator([
+  'gmail.com',
+  'yahoo.com',
+  'hotmail.com',
+]);
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.scss'
+  styleUrl: './contact-us.component.scss',
 })
 export class ContactUsComponent {
+  contactForm = new FormGroup({
+    senderName: new FormControl('', [Validators.required]),
+    senderEmail: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      invalidEmailDomain,
+    ]),
+    senderMessage: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+  });
 
+  submitForm() {
+    console.log(this.contactForm.valid);
+    this.contactForm.reset();
+  }
 }
