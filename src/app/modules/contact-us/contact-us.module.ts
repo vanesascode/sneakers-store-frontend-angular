1b4ad0572fc831ec77e6608d6b462a4cc75e01e9
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-03 19:45:51 +0200

    feat: :sparkles: add primeng UI modules in the contact-us module file

--- src/app/modules/contact-us/contact-us.module.ts
+++ src/app/modules/contact-us/contact-us.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { InputTextModule } from 'primeng/inputtext';
 import { ContactUsRoutingModule } from './contact-us-routing.module';
 import { ContactUsComponent } from './contact-us/contact-us.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { EditorModule } from 'primeng/editor';
+import { ButtonModule } from 'primeng/button';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @NgModule({
-  declarations: [
-    ContactUsComponent
-  ],
+  declarations: [ContactUsComponent],
   imports: [
     CommonModule,
-    ContactUsRoutingModule
-  ]
+    ContactUsRoutingModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    EditorModule,
+    ButtonModule,
+    InputTextareaModule,
+  ],
 })
-export class ContactUsModule { }
+export class ContactUsModule {}
