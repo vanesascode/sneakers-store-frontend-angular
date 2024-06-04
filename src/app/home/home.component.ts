28447e2ab422b9c72fd8a3c0068fc5b0f4a9c411
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-04 12:26:52 +0200

    refactor: :recycle: remove last slash in the links in the home component file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -81,7 +81,7 @@ export class HomeComponent {
   fetchProducts(page: number, perPage: number) {
     this.productsService
       .getProducts(
-        'https://sneakers-store-backend-express.onrender.com/clothes/',
+        'https://sneakers-store-backend-express.onrender.com/clothes',
         {
           page,
           perPage,
@@ -101,7 +101,7 @@ export class HomeComponent {
   editProduct(product: Product, id: number) {
     this.productsService
       .editProduct(
-        'https://sneakers-store-backend-express.onrender.com/clothes/' + id,
+        'https://sneakers-store-backend-express.onrender.com/clothes' + id,
         product
       )
       .subscribe({
@@ -119,7 +119,7 @@ export class HomeComponent {
   deleteProduct(id: number) {
     this.productsService
       .deleteProduct(
-        'https://sneakers-store-backend-express.onrender.com/clothes/' + id
+        'https://sneakers-store-backend-express.onrender.com/clothes' + id
       )
       .subscribe({
         next: (data) => {
@@ -136,7 +136,7 @@ export class HomeComponent {
   addProduct(product: Product) {
     this.productsService
       .addProduct(
-        'https://sneakers-store-backend-express.onrender.com/clothes/',
+        'https://sneakers-store-backend-express.onrender.com/clothes',
         product
       )
       .subscribe({
